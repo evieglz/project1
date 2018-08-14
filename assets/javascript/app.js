f6dae1117345586a716537e61f797e91081cfdfd
World Wide <agwalker249@gmail.com>
2018-08-14 14:51:56 -0500

    firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,19 @@
 /* map */
- 
+
 ///////////////
+
+var config = {
+  apiKey: "AIzaSyD7SxusedMqWMEMSF6-nPQf_5JvcoMlD_k",
+  authDomain: "project1-ffdfe.firebaseapp.com",
+  databaseURL: "https://project1-ffdfe.firebaseio.com",
+  projectId: "project1-ffdfe",
+  storageBucket: "project1-ffdfe.appspot.com",
+  messagingSenderId: "177189113878"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 // This example adds a search box to a map, using the Google Place Autocomplete
 // feature. People can enter geographical searches. The search box will return a
@@ -10,7 +23,7 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 30.267153, lng: -97.7430608},
+    center: { lat: 30.267153, lng: -97.7430608 },
     zoom: 15,
     mapTypeId: 'roadmap'
   });
@@ -19,28 +32,62 @@ function initAutocomplete() {
   var searchBox = new google.maps.places.SearchBox(input);
   //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+  map.addListener('bounds_changed', function () {
     searchBox.setBounds(map.getBounds());
   });
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+
+  // $("#infoTable").empty();
+  searchBox.addListener('places_changed', function () {
+   
     var places = searchBox.getPlaces();
 
     console.log(places);
+
+    // forloop through places.length
+
+    for (var i = 0; i < places.length; i++) {
+
+
+      var objectIWantToSendUp = {
+        "formatted_addresses": places[i].formatted_address,
+        "name": places[i].name
+      }
+
+
+
+      database.ref().push(objectIWantToSendUp);
+    }
+      // *****New Stuff*****
+
+      database.ref().on("child_added", function (snapshot) {
+        var name = snapshot.val().name;
+        var addresses = snapshot.val().formatted_address;
+
+        $("#infoTable > tBody").append("<tr><td>" + name + "</td><td>" + addresses + "</td></tr>");
+
+      })
+
+
+
+
+      // *****New Stuff*****
+
+   
 
     if (places.length == 0) {
       return;
     }
     // Clear out the old markers.
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
       marker.setMap(null);
     });
     markers = [];
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
+    places.forEach(function (place) {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
